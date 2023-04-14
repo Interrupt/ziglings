97b89c68c4ceae65a647d2d4b1dbd27bae398cfc
Chad Cuddigan <cuddigan@gmail.com>
2023-04-14 10:16:10 -0700

    Finishing the rest of the exercises

--- exercises/098_bit_manipulation2.zig
+++ exercises/098_bit_manipulation2.zig
@@ -60,5 +60,5 @@ fn isPangram(str: []const u8) bool {
     // and if so, we know the given string is a pangram
     //
     // but what do we have to compare?
-    return bits == 0x..???;
+    return bits == 0x3FFFFFF; // Three full bytes (FF*3) = 24 bits, plus two extra bits (11 = 3)
 }
